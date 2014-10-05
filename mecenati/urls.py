90323b44be1e5351ef5d1262a987049aa58e4661
soujiro <kivandrow@hotmail.com>
2014-10-05 14:57:48 +0200

    added pages

--- mecenati/urls.py
+++ mecenati/urls.py
@@ -9,6 +9,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('profiles.urls')),
     url(r'^contests/', include('auction.urls')),
+    url(r'^artists/$', TemplateView.as_view(template_name="mecenati/artists.html")),
+    url(r'^artists/bansky/$', TemplateView.as_view(template_name="mecenati/bansky.html")),
+    url(r'^patrons/contests/$', TemplateView.as_view(template_name="mecenati/contests.html")),
     url(r'^$', TemplateView.as_view(template_name="mecenati/home.html")),
 )
 
